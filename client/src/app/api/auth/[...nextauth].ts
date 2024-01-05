3bbe4d7649d74df79f9d433b3155477079d541a4
jp-quintana <juanquintana1996@gmail.com>
2024-01-04 23:57:48 -0300

    wip

--- client/src/app/api/auth/[...nextauth].ts
+++ client/src/app/api/auth/[...nextauth].ts
@@ -1,34 +1,34 @@
-// import NextAuth from 'next-auth';
+import NextAuth from 'next-auth';
 
-// const options = {
-//   providers: [],
-//   callbacks: {
-//     jwt: async ({ token, user }) => {
-//       if (user) {
-//         token = {
-//           ...token,
-//           id: user.id,
-//           username: user.username,
-//           accessToken: user.accessToken,
-//         };
-//       }
-//       return token;
-//     },
-//     session: async ({ session, token }) => {
-//       if (token) {
-//         session.user = {
-//           id: token.id,
-//           username: token.username,
-//         };
-//         session.accessToken = token.accessToken;
-//       }
-//       return session;
-//     },
-//   },
-//   secret: 'your-secret-key',
-//   session: {
-//     jwt: true,
-//   },
-// };
+const options = {
+  providers: [],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token = {
+          ...token,
+          id: user.id,
+          username: user.username,
+          accessToken: user.accessToken,
+        };
+      }
+      return token;
+    },
+    session: async ({ session, token }) => {
+      if (token) {
+        session.user = {
+          id: token.id,
+          username: token.username,
+        };
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
+  secret: 'your-secret-key',
+  session: {
+    jwt: true,
+  },
+};
 
-// export default (req, res) => NextAuth(req, res, options);
+export default (req, res) => NextAuth(req, res, options);
