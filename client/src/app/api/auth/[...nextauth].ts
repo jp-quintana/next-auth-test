ca82a770b9edd0cc9c45bbf0ae29c75834cc3931
jp-quintana <juanquintana1996@gmail.com>
2024-01-06 21:45:23 -0300

    wip

--- client/src/app/api/auth/[...nextauth].ts
+++ client/src/app/api/auth/[...nextauth].ts
@@ -30,33 +30,33 @@ const options = {
       },
     }),
   ],
-  callbacks: {
-    jwt: async ({ token, user }) => {
-      if (user) {
-        token = {
-          ...token,
-          id: user.id,
-          username: user.username,
-          accessToken: user.accessToken,
-        };
-      }
-      return token;
-    },
-    session: async ({ session, token }) => {
-      if (token) {
-        session.user = {
-          id: token.id,
-          username: token.username,
-        };
-        session.accessToken = token.accessToken;
-      }
-      return session;
-    },
-  },
-  secret: 'your-secret-key',
-  session: {
-    jwt: true,
-  },
+  // callbacks: {
+  //   jwt: async ({ token, user }) => {
+  //     if (user) {
+  //       token = {
+  //         ...token,
+  //         id: user.id,
+  //         username: user.username,
+  //         accessToken: user.accessToken,
+  //       };
+  //     }
+  //     return token;
+  //   },
+  //   session: async ({ session, token }) => {
+  //     if (token) {
+  //       session.user = {
+  //         id: token.id,
+  //         username: token.username,
+  //       };
+  //       session.accessToken = token.accessToken;
+  //     }
+  //     return session;
+  //   },
+  // },
+  // secret: 'your-secret-key',
+  // session: {
+  //   jwt: true,
+  // },
 };
 
-export default (req, res) => NextAuth(req, res, options);
+// export default (req, res) => NextAuth(req, res, options);
