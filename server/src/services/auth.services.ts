d5ffaf2fa9a277b9c0869093fe6c51c2e0312384
jp-quintana <juanquintana1996@gmail.com>
2024-01-12 21:59:37 -0300

    wip

--- server/src/services/auth.services.ts
+++ server/src/services/auth.services.ts
@@ -2,27 +2,28 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import User from '../dao/user';
 import CustomError from '../models/error.model';
+import { UserCredentials, UserRegisterCredentials } from '../types/types';
 
-export const register = async (userDetails) => {
-  // const existingUser = await User.fetchByEmail(userDetails.email);
-  // if (existingUser) {
-  //   throw new CustomError('Email address is already in use.', 409);
-  // }
-  // const { name, lastName, email, password } = userDetails;
-  // const salt = await bcrypt.genSalt(10);
-  // const encryptedPassword = await bcrypt.hash(password, salt);
-  // const newUser = await User.create({
-  //   name,
-  //   lastName,
-  //   email,
-  //   password: encryptedPassword,
-  // });
-  // const payload = { user: { id: newUser.id } };
-  // return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '365d' });
+export const register = async (user: UserRegisterCredentials) => {
+  const existingUser = await User.fetchByEmail(user.email);
+  if (existingUser) {
+    throw new CustomError('Email address is already in use.', 409);
+  }
+  const { name, lastName, email, password } = user;
+  const salt = await bcrypt.genSalt(10);
+  const encryptedPassword = await bcrypt.hash(password, salt);
+  const newUser = await User.create({
+    name,
+    lastName,
+    email,
+    password: encryptedPassword,
+  });
+  const payload = { user: { id: newUser.id } };
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '365d' });
 };
 
-export const login = async (userCredentials) => {
-  const { email, password } = userCredentials;
+export const login = async (user: UserCredentials) => {
+  const { email, password } = user;
 
   const existingUser = await User.fetchByEmail(email);
 
--- server/src/services/user.services.ts
+++ server/src/services/user.services.ts
@@ -1,7 +1,7 @@
 import User from '../dao/user';
 import CustomError from '../models/error.model';
 
-export const fetchProfile = async (userId) => {
+export const fetchProfile = async (userId: string) => {
   const user = await User.fetchById(userId);
 
   if (!user) {
