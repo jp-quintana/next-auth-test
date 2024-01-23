ac05e90e0066cf3b71bdd43b778e1e0ded31b46f
jp-quintana <juanquintana1996@gmail.com>
2024-01-22 23:58:56 -0300

    wip3: nav ui

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import Link from "next/link";
 
 const Nav = () => {
   return (
-    <nav className="mx-auto flex max-w-6xl justify-between">
+    <nav className="mx-auto flex max-w-6xl justify-between py-3">
       <p>logo</p>
-      <div></div>
+      <ul className="flex gap-6">
+        <li>
+          <Link href="/sign-in">sign in</Link>
+        </li>
+        <li>
+          <Link href="/sign-in">sign up</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
