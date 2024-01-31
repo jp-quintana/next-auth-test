1b92f996169ba378df40153e477a36ad99c72058
jp-quintana <juanquintana1996@gmail.com>
2024-01-30 23:56:32 -0300

    wip

--- client/src/components/nav.tsx
+++ client/src/components/nav.tsx
@@ -4,7 +4,7 @@ import { buttonVariants } from "@/components/ui/button";
 const Nav = () => {
   return (
     <nav className="mx-auto flex max-w-6xl items-center justify-between py-3">
-      <p>next-auth-test</p>
+      <p className="font-bold">next-auth-test</p>
       <ul className="flex items-center gap-6">
         <li>
           <Link
